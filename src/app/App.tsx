87b3d6e709ed90e337c3e7fec89cde1deb00e05b
DigiverseDeveloper <josephine@otimi.com.au>
2026-05-20 15:32:14 +1000

    Wire driver delivery Zoho bridge

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import SecureLoginBridge from './SecureLoginBridge.jsx';
 import ZohoDealBridge from './ZohoDealBridge.jsx';
 import DriverWorkflowBridge from './DriverWorkflowBridge.jsx';
 import DriverPickupOutcomeBridge from './DriverPickupOutcomeBridge.jsx';
+import DriverDeliveryOutcomeBridge from './DriverDeliveryOutcomeBridge.jsx';
 import ClientOrderDateBridge from './ClientOrderDateBridge.jsx';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <MotoCoLogistics />
         <DriverWorkflowBridge />
         <DriverPickupOutcomeBridge />
+        <DriverDeliveryOutcomeBridge />
         <ClientOrderDateBridge />
       </ZohoDealBridge>
     </SecureLoginBridge>
